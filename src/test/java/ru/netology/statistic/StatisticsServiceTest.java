c83225a210bfd08755e0b9fbd90d6081c405922e
PetrIvChe <cherniavskiipetr@gmail.com>
2021-10-19 13:17:24 +0300

    Made reformate code, delyte cycle from the test, chenched  array's values

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatisticsServiceTest {
 
@@ -17,17 +17,18 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
     @Test
     void findMax1() {
         StatisticsService service = new StatisticsService();
-            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-            for (long i = 0; i < incomesInBillions.length; i++) {
+        long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        // for (long i = 0; i < incomesInBillions.length; i++) {
 
-            long expected = 12;
+        long expected = 12;
 
-            long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
-            assertEquals(expected, actual);
-        }
+        assertEquals(expected, actual);
     }
 }
+
